5b9c117d801627fd74e80ed5611abf6da074da54
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2017-11-10 20:04:13 +0900

    4問目

---
+++ kadai4/src/kadai4/Main.java
@@ -0,0 +1,17 @@
+package kadai4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 3;
+		
+		if (a % 2 == 0) {
+			System.out.println("偶数");
+		}
+		else {
+			System.out.println("奇数");
+		}
+	}
+
+}

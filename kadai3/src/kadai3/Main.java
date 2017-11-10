da228ca5e2c125ea4585a801672f903d00f0d9f9
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2017-11-10 19:58:52 +0900

    3問目

---
+++ kadai3/src/kadai3/Main.java
@@ -0,0 +1,15 @@
+package kadai3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a, i;
+		a = 5;
+		
+		for (i = 0; i <= a; i++) {
+			System.out.print(i+" ");
+		}
+	}
+
+}

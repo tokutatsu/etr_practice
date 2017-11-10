0ea685adbcc70c5936ce1ba0fbfb164392ca42b9
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2017-11-10 19:48:01 +0900

    二問目

---
+++ kadai2/src/kadai2/Main.java
@@ -0,0 +1,19 @@
+package kadai2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a, b;
+		
+		a = 4;
+		b = 2;
+		
+		System.out.println("足し算:" + (a + b));
+		System.out.println("引き算:" + (a - b));
+		System.out.println("掛け算:" + (a * b));
+		System.out.println("割り算:" + (a / b));
+		
+	}
+
+}
